9f61b16e6436a1c9544dc2c9686055759f192808
hunghungdungdung <75792142+hunghungdungdung@users.noreply.github.com>
2023-02-03 15:46:12 +0700

    Create bai6.cpp

---
+++ bai6.cpp
@@ -0,0 +1,19 @@
+/*Bài 6: Tính S(n) = 1/1×2 + 1/2×3 +…+ 1/n x (n + 1)*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a=0;
+    double n;
+    double S=0;
+    cout << "Nhap so muon duoc nhan doi va chia boi 1 tu 1/2 den:" << endl;
+    cin >> n;
+    while (a<n)
+    {
+        a++;
+        S = S+(1/(a*(a+1)));
+        
+    }
+    cout << "Dap an la: " << S;
+    return 0;
+}
